91eab622d01c7a9d3e52a02db881b1a14d43e24e
Steve Ortiz <steve@ortizaggies.com>
2016-11-20 21:54:16 -0600

    remove outdated links to other pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,16 @@
 module ApplicationHelper
-  @@nav_links = %w{sample demo navair business}
-  @@nav_names = ["Sample Work", "Chess Demo", "NAVAIR", "Business Solutions"]
+  # note: this used to have several page links, but I'm only keeping the demo here
+  @@nav_links = %w{demo}
+  @@nav_names = ["Chess Demo"]
   @@nav_hash = Hash[@@nav_links.zip @@nav_names]
   def navbar(current)
     result = '<ul class="nav navbar-nav">'
     @@nav_links.each_index do |x|
       if @@nav_links[x] == current
-        result+= '<li class="active">'
+        result += "<li class=\"active\"><a href=\"#\">#{@@nav_names[x]}</a></li>"
       else
-        result+= '<li>'
+        result += "<li><a href=\"#{@@nav_links[x]}\">#{@@nav_names[x]}</a></li>"
       end
-      result += "<a href=\"#{@@nav_links[x]}\">#{@@nav_names[x]}</a></li>"
     end
     result += '</ul>'
     return raw(result)
