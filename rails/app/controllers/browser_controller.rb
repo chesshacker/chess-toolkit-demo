f07889ae8b4ab3490299a3015d3c349d4c8db336
Steve Ortiz <steve@ortizaggies.com>
2016-12-25 15:09:36 -0500

    disable csrf

--- rails/app/controllers/browser_controller.rb
+++ rails/app/controllers/browser_controller.rb
@@ -1,9 +1,4 @@
 class BrowserController < ApplicationController
-  after_filter :cors_set_access_control_headers
-  def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'GET'
-  end
   def index
     @current = 'demo'
     @graph = CtGraph.new
